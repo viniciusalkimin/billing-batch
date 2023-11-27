5bb36a734c91aeaaf4ea847bb4e1dde46a0a4b20
viniciusalkimin <viniciusg_alkimin@hotmail.com>
2023-11-27 12:47:37 -0300

    Initial project

---
+++ src/main/java/com/alkimin/billingjob/infra/batch/BillingJobConfiguration.java
@@ -0,0 +1,7 @@
+package com.alkimin.billingjob.infra.batch;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BillingJobConfiguration {
+}
---
+++ src/test/java/com/alkimin/billingjob/BillingJobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alkimin.billingjob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillingJobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
